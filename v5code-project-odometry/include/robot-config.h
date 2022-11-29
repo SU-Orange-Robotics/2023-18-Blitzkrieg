52f344f7ff7c114be0ec0101ab87677bd52737fb
Zhuowei Zhang <zhuoweiz10@gmail.com>
2022-11-29 18:57:03 -0500

    unzip

---
+++ v5code-project-odometry/include/robot-config.h
@@ -0,0 +1,15 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor Motor15;
+extern motor Motor16;
+extern rotation Rotation1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
---
+++ v5code-project-odometry/src/robot-config.cpp
@@ -0,0 +1,26 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor Motor15 = motor(PORT15, ratio18_1, false);
+motor Motor16 = motor(PORT16, ratio18_1, true);
+rotation Rotation1 = rotation(PORT1, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
