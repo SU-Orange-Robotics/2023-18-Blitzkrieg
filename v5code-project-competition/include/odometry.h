da2e9a40f9bd259c6ddae6632d39f527ebf655e9
Orange Robotics <suorangerobotics@gmail.com>
2023-03-30 19:02:06 -0400

    this is how to include odometry as global variable

---
+++ v5code-project-competition/include/odometry.h
@@ -0,0 +1,51 @@
+#ifndef ODOMETRY_H
+#define ODOMETRY_H
+
+#include <vector>
+#include <string>
+// #include <string.h>
+#include <math.h>
+
+// #include "vex.h"
+
+using namespace vex;
+
+using namespace std;
+
+
+
+class Odometry {
+
+private:
+  double encoderLeft;
+  double encoderRight;
+  double encoderCenter;
+
+  double locationX;
+  double locationY;
+  double locationTheta;
+
+// measure the robot to set this
+  const double width = 5.0;   //6.5, distance from center of bot -> back tracking wheel
+  const double length = 16.7; //13.8, distance between center of left and rigtht wheels
+  const double circ = 3.8 * M_PI;
+
+
+public:
+  Odometry();
+
+  // aginst rite side white lane. facing up. (x = 86, y = 8, t = pi/2)
+  void reset();
+
+  double getX();
+  double getY();
+  double getTheta();
+
+  vector<double> getLocation();
+
+  void printLocation();
+
+  void updateOdometry();
+};
+
+#endif
